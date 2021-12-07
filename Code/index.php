87785d264865f12c905c65b3a39ce9f4e43699a3
mrvic <mrvic.predrag@gmail.com>
2021-12-07 21:10:15 +0100

    composer instalacija Faker

--- Code/index.php
+++ Code/index.php
@@ -3,9 +3,25 @@
 require 'vendor/autoload.php';
 
 use Carbon\Carbon;
-
+use Faker\Factory;
 
 printf("Now: %s", Carbon::now());
 
+echo "<br>";
 
+$faker = Faker\Factory::create();
+// generate data by calling methods
+echo $faker->name();
+echo "<br>";
+echo $faker->creditCardNumber;
+echo "<br>";
+echo $faker->email;
 //phpinfo();
+
+$x = new Faker();
+// generate data by calling methods
+echo $x->name();
+echo "<br>";
+echo $x->creditCardNumber;
+echo "<br>";
+echo $x->email;
