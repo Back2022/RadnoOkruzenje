a7085451009ba978cf08248043a4d42a86137fee
mrvic <mrvic.predrag@gmail.com>
2021-12-09 18:43:07 +0100

    dodani primjeri PHP osnove

---
+++ Code/PHPHOsnove/action.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+ime3=%23000000&ime4=&ime5=&ime6=pmrvic%40789.com&ime7=&ime8=&ime10=&ime11=&
+ime12=1234&range1=30&range-value=&ime16=&ime17=Submit&ime18=&ime19=&ime20=&
+ime21=https%3A%2F%2Flocalhost%3A8000%2FPHPHOsnove%2Fforme-specijal.html&ime22= 
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        var_dump($_GET);
+        print_r($_GET);
+        ?>
+    </body>
+</html>
