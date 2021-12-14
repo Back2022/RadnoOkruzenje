01939c7be8a6c0d805cb88a4ee4e56623e4ca77c
mrvic <mrvic.predrag@gmail.com>
2021-12-14 20:34:30 +0100

    foreach

--- Code/Primjeri_PHP_osnove/Kontrolne_strukture/Petlje/for.php
+++ Code/Primjeri_PHP_osnove/Kontrolne_strukture/Petlje/for.php
@@ -1,5 +1,54 @@
 <?php 
+echo "<hr>nacin 1<br>";
 for ($i = 1; $i < 10; $i++) {    
     echo $i . " "; 
+}
+
+echo "<hr>nacin 2 unatrag <br>";
+for ($i = 10; $i > 0; $i--) {    
+    echo $i . " "; 
+}
+echo "<hr>nacin 3 unatrag neparni<br>";
+for ($i = 33; $i > 0; $i-=2) {    
+    echo $i . " "; 
+}
+echo "<hr>nacin 4 unatrag neparni<br>";
+for ($i = 33; $i > 0; $i--) {  
+    if($i%2==0){
+        continue;
+    }
+    echo $i . " "; 
+}
+
+echo "<hr>nacin 5 unatrag neparni<br>";
+for ($i = 33; $i > 0; $i--) {  
+    if($i%2!=0){
+           echo $i . " "; 
+    }
+}
+
+echo "<hr>nacin 5 unatrag neparni od 33 do 13 <br>";
+for ($i = 33; $i > 13; $i--) {  
+    if($i%2!=0){
+           echo $i . " "; 
+    }
+}
+echo "<hr>nacin 5 unatrag neparni od 333 do 13 djeljivi sa 7 <br>";
+for ($i = 333; $i > 13; $i--) {  
+    if($i%7==0){
+           echo $i . " "; 
+    }
+}
+
+echo "<hr>djeljivi sa 7 i sa 3 <br>";
+for ($i = 3; $i < 633; $i++) {  
+    if($i%7==0 && $i%3==0){
+        if($i%9==0) {
+          echo "<span style='color:red'>$i</span>" . " ";   
+        }
+        else{
+        echo $i . " "; 
+        }
+    }
 }
 ?>
--- Code/Primjeri_PHP_osnove/Kontrolne_strukture/Petlje/foreach.php
+++ Code/Primjeri_PHP_osnove/Kontrolne_strukture/Petlje/foreach.php
@@ -1,9 +1,57 @@
 <?php 
 $names = ['Harry', 'Ron', 'Hermione']; 
 foreach ($names as $name) {    
-    echo $name . " "; 
+    echo $name . " <br>"; 
 } 
+
+echo "<hr>";
 foreach ($names as $key => $name) {   
-     echo $key . " -> " . $name . " "; 
+     echo $key . " -> " . $name . " <br>"; 
 }
+
+
+echo "<hr>";
+// neparni 1-33
+$neparni=range(1,33,2);
+//print_r($neparni);
+
+foreach ($neparni as $value) {
+    echo $value . " ";
+}
+
+echo "<hr>";
+// neparni 1-33
+$neparni=range(1,33,1);
+//print_r($neparni);
+
+foreach ($neparni as $value) {
+    echo $value . " ";
+}
+echo "<hr>";
+foreach ($neparni as $value) {
+    if($value%2!=0){         
+    echo $value . " "; 
+    }
+}
+echo "<hr>";
+foreach ($neparni as $value) {
+    if($value%2==0){         
+    continue;
+    }
+    echo $value . " "; 
+}
+
+$voce_boja=array(
+    'banana'=>'zuta',
+    'jabuka'=>'crvena',
+    'kivi'  =>'zelena',
+    'kokos' =>'smedja'
+    );
+
+echo "<ul>";
+    foreach($voce_boja as $voce=>$boja){
+        echo "<li>$voce je voce čija je boja $boja</li>";
+    }
+echo "</ul>";    
+
 ?>
